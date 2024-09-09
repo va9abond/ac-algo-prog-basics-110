540f039020339651b7d196cc601f057d240f43c9
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2024-09-10 01:37:58 +0300

    wip[tasks]: redo task cross using mark_line!

--- tasks/cross.jl
+++ tasks/cross.jl
@@ -3,7 +3,7 @@ include("../inc/roblib.jl")
 
 function cross!(robot::Robot)
     for side in [Nord,Ost,West,Sud]
-        steps_till_border::Integer = move_till_border!(robot, side, putmarker=true)
-        move_steps!(robot, inverse_side(side), steps_till_border, putmarker=false)
+        steps_in_side::Integer = mark_line!(robot, side)
+        move_steps!(robot, inverse_side(side), steps_in_side)
     end
 end
