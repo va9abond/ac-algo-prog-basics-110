8c4d74f471c1c5243f5d524321f9d64fa761874a
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2024-09-14 00:11:55 +0300

    fix: following the commit 75c915

--- tasks/cross.jl
+++ tasks/cross.jl
@@ -4,6 +4,6 @@ include("../inc/roblib.jl")
 function cross!(robot::Robot)
     for side in [Nord,Ost,West,Sud]
         steps_in_direction::Integer = mark_direction!(robot, side)
-        move!(robot, inverse_side(side), steps_in_direction)
+        move!(robot, reverse_side(side), steps_in_direction)
     end
 end
