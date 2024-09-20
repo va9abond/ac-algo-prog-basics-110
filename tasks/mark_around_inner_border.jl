ff3a85447ee9e0300e20b5166ac5d4a22cadba6b
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2024-09-20 23:06:31 +0300

    wip[tasks]: done mark around inner border

--- tasks/mark_around_inner_border.jl
+++ tasks/mark_around_inner_border.jl
@@ -42,26 +42,23 @@ end
 
 
 function mark_around_border!(robot::Robot)::Bool
-    # START: robot stay on the corner of inner border
-    direction_limit = which_border(robot)[2]
-    direction_move = Nord
-    for side in [West, Ost]
-        move!(robot, side)
-        (isborder(robot, direction_limit)) && (direction_move = side)
-        move!(robot, reverse_side(side))
-    end
+    # START: robot stay on the corner(Nord,Ost) of inner border
+    direction_border = Sud
+    direction_move = West
 
-    for _ in 1:4
+    # border should always be on "left hand side" of the robot - anticlocwise bypass
+    for _ in 0:3
         putmarker!(robot)
 
-        while (isborder(robot, direction_limit))
+        while (isborder(robot, direction_border))
             move!(robot, direction_move)
             putmarker!(robot)
         end
 
-        direction_move = direction_limit
+        direction_move = direction_border
         move!(robot, direction_move)
-        direction_limit = which_border(robot)[2]
+
+        direction_border = next_side(direction_border)
     end
 
     return true
@@ -71,12 +68,12 @@ end
 function main()
     robot::Robot = Robot("mark_around_inner_border.sit", animate=true)
 
-    corner = (Nord, West)
+    corner = (Nord, Ost)
     success, path_into_corner = move_into_corner!(robot, side_v=corner[1], side_h=corner[2])
     (!success) && (println("failed to reach the corner"), return false)
 
     # mark_perimeter!
-    side::HorizonSide = Sud
+    side::HorizonSide = reverse_side(corner[2])
     for _ in 0:3
         mark_direction!(robot, side) # mark from corner to corner
         side = next_side(side)
@@ -92,4 +89,6 @@ function main()
 
     success = move!(robot, reverse_path(path_into_corner))[1]
     (!success) && (println("failed to return to the starting position"), return false)
+
+    return true
 end
