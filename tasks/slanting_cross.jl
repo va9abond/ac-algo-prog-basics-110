459996a72106cc89ff72e2d487b79feab9148f14
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2024-09-20 09:39:03 +0300

    wip[tasks]: done slanting cross

---
+++ tasks/slanting_cross.jl
@@ -0,0 +1,12 @@
+include("../inc/roblib.jl")
+
+
+function slanting_cross!(robot::Robot)
+    # for (side_v, side_h) in [(Nord, West), (Nord, Ost), (Sud, West), (Sud, Ost)]
+    for side_v in [Nord, Sud]
+        for side_h in [West, Ost]
+            path_in_direction = mark_direction!(robot, side_v, side_h)
+            move!(robot, reverse_path(path_in_direction))
+        end
+    end
+end
