3499b0776b8331075a0dfadec6f12ff735ca8094
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2024-09-24 10:57:06 +0300

    wip[tasks]: done find_marker_borders

---
+++ tasks/find_marker_borders.jl
@@ -0,0 +1,27 @@
+include("../inc/roblib.jl")
+
+
+function main()
+    robot::Robot = Robot("find_marker_borders.sit", animate=true)
+
+    init_side::HorizonSide = Nord
+    flag_marker::Bool = false
+    (ismarker(robot)) && (flag_marker = true)
+    putmarker!(robot)
+
+
+    steps_in_direction::Int = 1
+    side::HorizonSide = init_side
+    while (!flag_marker)
+        (side == reverse_side(init_side)) && (steps_in_direction += 1)
+        
+        for i in (1:steps_in_direction)
+            move_through_borders!(robot, side)
+            (ismarker(robot)) && (flag_marker = true) && (break)
+        end
+
+        side = next_side(side)
+        (side == init_side) && (steps_in_direction += 1)
+    end
+end
+
