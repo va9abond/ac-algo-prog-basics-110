05aeb9cd0a25859137db3151316cf983af4bd04e
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2024-10-20 18:08:14 +0300

    ref: 2-mark_perimeter done

--- tasks/2-mark_perimeter.jl
+++ tasks/2-mark_perimeter.jl
@@ -1,50 +1,41 @@
 include("../inc/roblib.jl")
 
 
-function move_into_corner!(robot::Robot; side_v::HorizonSide=Nord, side_h::HorizonSide=West)::Tuple{Bool, Vector{Tuple{HorizonSide, Integer}}}
-    path::Vector{Tuple{HorizonSide, Integer}} = []
-
-    for side in [side_v, side_h]
-        steps = move!(isborder, robot, side)
-        push!(path, (side, steps))
+function mark_perimeter!(robot)
+    # determining which corner the robot is in
+    corner = []
+    for side in [Nord, Ost]
+        border_side = isborder(robot, side) ? side : reverse_side(side)
+        push!(corner, border_side)
     end
 
-    (!isborder(robot,side_v) || !isborder(robot,side_h)) && (return (false, path))
-    return (true, path)
-end
-
-
-function mark_perimeter!(robot::Robot)::Bool
-    corner = @NamedTuple{side_v::HorizonSide, side_h::HorizonSide}((Nord, West))
-    success, path_into_corner = move_into_corner!(robot, side_v=corner.side_v, side_h=corner.side_h)
-    (!success) && (println("failed to reach the corner"), return false)
-
-    # Int(side_v) + Int(side_h) == [1,3]
-    # + ----------- +
-    # |1           3|
-    # |             |
-    # |             |
-    # |3           1|
-    # + ----------- +
-    corner_sum::Integer = (Int(corner.side_v) + Int(corner.side_h)) % 4
-    side_start::HorizonSide = Nord # TODO default invalid value
+    # Int(corner[1]) + Int(corner[2]) == [1,3]
+    # + ------ +
+    # |1      3|
+    # |        |
+    # |3      1|
+    # + ------ +
+    corner_sum::Int = mod((Int(corner[1]) + Int(corner[2])), 4)
+    move_side::HorizonSide = West
     if (corner_sum == 1)
-        side_start = reverse_side(corner.side_v)
+        move_side = reverse_side(corner[1])
     else
-        side_start = reverse_side(corner.side_h)
+        move_side = reverse_side(corner[2])
     end
 
-    for i in 0:3
-        local side::HorizonSide = HorizonSide((Int(side_start)+i) % 4)
-        mark_direction!(robot, side) # mark from corner to corner
+    for _ in 0:3
+        mark_direction!(robot, move_side)
+        move_side = next_side(move_side)
     end
 
-    success = move!(robot, reverse_path(path_into_corner))[1]
-    (!success) && (println("failed to return to the starting position"), return false)
-
-    return true
+    return nothing
 end
 
 
+function main!()
+    robot = Robot("random_pos_no_borders.sit", animate=true)
 
-
+    path_into_corner = move_into_corner!(robot, (Nord, Ost))
+    mark_perimeter!(robot)
+    move!(robot, reverse_path(path_into_corner))
+end
