478b6605c51df7980dfc0a103dfc9f9522b96501
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2024-10-20 16:54:23 +0300

    ref: 1-cross done

--- tasks/1-cross.jl
+++ tasks/1-cross.jl
@@ -1,9 +1,15 @@
 include("../inc/roblib.jl")
 
 
-function cross!(robot::Robot)
-    for side in [Nord,Ost,West,Sud]
-        steps_in_direction::Integer = mark_direction!(robot, side)
+function cross!(robot)
+    for side in [Nord, Ost, West, Sud]
+        steps_in_direction::Int = mark_direction!(robot, side)
         move!(robot, reverse_side(side), steps_in_direction)
     end
+end
+
+
+function main!()
+    robot::Robot = Robot("1-cross.sit", animate=true)
+    cross!(robot)
 end
