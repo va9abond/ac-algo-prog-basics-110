099052c8f8bf990c7b20f7621a69027d4dd273f9
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2024-10-21 21:43:19 +0300

    ref: 4-slanting_cross done

--- tasks/4-slanting_cross.jl
+++ tasks/4-slanting_cross.jl
@@ -1,7 +1,7 @@
 include("../inc/roblib.jl")
 
 
-function slanting_cross!(robot::Robot)
+function slanting_cross!(robot)::Nothing
     # for (side_v, side_h) in [(Nord, West), (Nord, Ost), (Sud, West), (Sud, Ost)]
     for side_v in [Nord, Sud]
         for side_h in [West, Ost]
@@ -9,4 +9,17 @@ function slanting_cross!(robot::Robot)
             move!(robot, reverse_path(path_in_direction))
         end
     end
+
+    return nothing
+end
+
+
+function main!()
+    robot = Robot("random_pos_no_borders.sit", animate=true)
+    slanting_cross!(robot)
+end
+
+
+function main!(robot)
+    slanting_cross!(robot)
 end
