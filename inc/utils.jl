8dde56e0a019ec2897fb34c161322e5f973d87d6
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2024-10-21 20:40:29 +0300

    wip: add WARN()

--- inc/utils.jl
+++ inc/utils.jl
@@ -2,3 +2,9 @@ function VERIFY(cond::Bool, msg::String)
     (!cond) && print("\033[1;31m", msg, "\033[m", "\n")
     return nothing
 end
+
+
+function WARN(msg::String)::Nothing
+    print("\033[1;31m", msg, "\033[m", "\n")
+    return nothing
+end
