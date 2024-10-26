d950cb543498e9238f51c442808c70cdb1af81c1
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2024-10-26 13:28:27 +0300

    ref: 7-find_door done

--- tasks/7-find_door.jl
+++ tasks/7-find_door.jl
@@ -1,22 +1,17 @@
 include("../inc/roblib.jl")
 
 
-function main()
-    robot::Robot = Robot("find_door.sit", animate=true)
+function main!()
+    robot::Robot = Robot("7-find_door.sit", animate=true)
     putmarker!(robot)
 
-    flag_door::Bool = false
-    (!isborder(robot, Nord)) && (flag_door = true)
+    swing!(()->!isborder(robot, Nord), robot)
+end
 
-    steps_in_direction::Int = 1
-    side::HorizonSide = next_side(Nord)
-    while (!flag_door)
-        move!(robot, side, steps_in_direction)
-        (!isborder(robot, Nord)) && (flag_door = true)
-        steps_in_direction += 1
-        side = reverse_side(side)
-    end
 
-    move!(robot, Nord)
+function main!(robot)
+    putmarker!(robot)
+
+    swing!(()->!isborder(robot, Nord), robot)
 end
 
