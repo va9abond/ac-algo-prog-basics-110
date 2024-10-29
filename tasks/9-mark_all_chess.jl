2a4a252c8b3e64a37bdcddc599c7477c03bb3ae7
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2024-10-29 15:27:05 +0300

    ref: 9-mark_all_chess done

--- tasks/9-mark_all_chess.jl
+++ tasks/9-mark_all_chess.jl
@@ -1,30 +1,36 @@
 include("../inc/roblib.jl")
 
 
-function mark_chess_whole_field!(robot::Robot)::Bool
-    corner = (Nord, West)
-    success, path_into_corner = move_into_corner!(robot, side_v=corner[1], side_h=corner[2])
-    (!success) && (println("failed to reach the corner"), return false)
+function mark_all_chess!(robot::Robot, corner::NTuple{2, HorizonSide}, init_parity::Int)::Nothing
+    !iscorner(robot) && (WARN("robot is not in the corner"), return nothing)
 
-    direction_limit::HorizonSide = reverse_side(corner[1])
-    direction::HorizonSide = reverse_side(corner[2])
+    direction_border::HorizonSide = reverse_side(corner[1])
+    direction_in_row::HorizonSide = reverse_side(corner[2])
+    parity::Int = init_parity
 
-    parity::Int8 = 0
-    parity = mark_chess_direction!(robot, direction, parity)
-    while (!isborder(robot, direction_limit))
-        move!(robot, direction_limit)
+    parity = mark_chess_direction!(robot, direction_in_row, parity)
+    while (!isborder(robot, direction_border))
+        move!(robot, direction_border)
+
         parity = (parity+1) % 2
+        direction_in_row = reverse_side(direction_in_row)
 
-        direction = reverse_side(direction)
-        parity = mark_chess_direction!(robot, direction, parity)
+        parity = mark_chess_direction!(robot, direction_in_row, parity)
     end
 
-    success = move_into_corner!(robot, side_v=corner[1], side_h=corner[2])[1]
-    (!success) && (println("failed to reach the corner"), return false)
-
-    success = move!(robot, reverse_path(path_into_corner))[1]
-    (!success) && (println("failed to return to the starting position"), return false)
-
-    return true
+    return nothing
 end
 
+
+function main!()
+    robot::Robot = Robot("random_pos_no_borders.sit", animate=true)
+    corner = (Nord, Ost)
+
+    path_into_corner = move_into_corner!(robot, corner)
+
+    parity::Int = mod(sum((p) -> p[2], path_into_corner), 2)
+    mark_all_chess!(robot, corner, 1-parity)
+
+    move_into_corner!(robot, corner)
+    move!(robot, reverse_path(path_into_corner))
+end
