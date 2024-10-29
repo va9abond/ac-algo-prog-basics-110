8f52678fbee8e8a175d834ecc335f1c0e1e9a2d7
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2024-10-29 14:36:09 +0300

    ref: 8-find_marker_spiral done

--- tasks/8-find_marker_spiral.jl
+++ tasks/8-find_marker_spiral.jl
@@ -1,26 +1,12 @@
 include("../inc/roblib.jl")
 
 
-function main()
-    robot::Robot = Robot("find_marker.sit", animate=true)
-
-    init_side::HorizonSide = Nord
-    flag_marker::Bool = false
-    (ismarker(robot)) && (flag_marker = true)
-    putmarker!(robot)
+function main!()
+    robot::Robot = Robot("8-find_marker_spiral.sit", animate=true)
+    move_spiral!(()->ismarker(robot), robot, Ost)
+end
 
 
-    steps_in_direction::Int = 1
-    side::HorizonSide = init_side
-    while (!flag_marker)
-        (side == reverse_side(init_side)) && (steps_in_direction += 1)
-        
-        for i in (1:steps_in_direction)
-            move!(robot, side)
-            (ismarker(robot)) && (flag_marker = true) && (break)
-        end
-
-        side = next_side(side)
-        (side == init_side) && (steps_in_direction += 1)
-    end
+function main!(robot)
+    move_spiral!(()->ismarker(robot), robot, Ost)
 end
