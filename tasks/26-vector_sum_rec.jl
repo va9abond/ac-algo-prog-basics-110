d0dd98ae0770d83697259012ed58267fed04721f
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2024-11-29 19:41:58 +0300

    wip: update 26-vector_sum_rec

--- tasks/26-vector_sum_rec.jl
+++ tasks/26-vector_sum_rec.jl
@@ -2,9 +2,18 @@ using Random
 using BenchmarkTools
 
 
-function sumrec(vector::Vector{Int})::Int
-    (length(vector) == 1) && return vector[1]
-    return vector[1] + sumrec(vector[2:end])
+function sumrec_x(vector::Vector{value_type})::value_type where value_type
+    len = length(vector)
+    (len == 1) && return vector[1]
+
+    pivot = div(len, 2)
+    return sumrec_x(vector[begin:pivot]) + sumrec_x(vector[pivot+1:end])
+end
+
+
+function sumrec(vector::Vector{value_type}, acc::value_type = 0)::value_type where value_type
+    isempty(vector) && return acc
+    return sumrec(vector[begin:end-1], acc + vector[end])
 end
 
 
@@ -12,6 +21,12 @@ end
 vec = Vector{Int}(undef, 10000)
 rand!(vec, -1000:1000)
 (length(vec) < 15) && println(transpose(vec))
+println(sum(vec))
+
+@time sum(vec)
+@time sumrec(vec)
+@time sumrec_x(vec)
+
 
 # Call built-in function
 # @show @benchmark sum(vec)
