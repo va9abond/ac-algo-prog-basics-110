f7193dbe0cb41e5cf15d618daf0555c1f1cb08c4
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2024-12-02 22:36:10 +0300

    wip: + dispatching constants directly

--- tasks/25-mark_chess_direction_rec.jl
+++ tasks/25-mark_chess_direction_rec.jl
@@ -1,12 +1,44 @@
 include("../inc/roblib.jl")
 
-
+# -----------
+# (i) variant
+# -----------
 function mark_chess_direction!(stop_cond::Function, robot, side::HorizonSide, parity::Int=1)
     if (!stop_cond())
         (parity == 1) && putmarker!(robot)
         move!(robot, side)
 
         return mark_chess_direction!(robot, side, 1-parity) do
+            stop_cond()
+        end
+    else
+        return nothing
+    end
+end
+
+
+# ------------
+# (ii) variant
+# ------------
+function mark_direction_chess!(stop_cond::Function, robot, side::HorizonSide, ::Val{0})
+    if (!stop_cond())
+        move!(robot, side)
+
+        return mark_direction_chess!(robot, side, Val(1)) do
+            stop_cond()
+        end
+    else
+        return nothing
+    end
+end
+
+
+function mark_direction_chess!(stop_cond::Function, robot, side::HorizonSide, ::Val{1})
+    if (!stop_cond())
+        putmarker!(robot)
+        move!(robot, side)
+
+        return mark_direction_chess!(robot, side, Val(0)) do
             stop_cond()
         end
     else
