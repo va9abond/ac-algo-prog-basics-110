da9d8e0897677305134afa4cbccfd3adcc7bd96b
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2024-12-17 23:11:01 +0300

    wip: add GeneralRobot

---
+++ inc/latest/GeneralRobot.jl
@@ -0,0 +1,18 @@
+using HorizonSideRobots
+include("AbstractRobot.jl")
+include("../roblib.jl")
+
+
+mutable struct GRobot <: AbstractRobot
+    _robot::Robot
+end
+getbaserobot(robot::GRobot) = robot._robot
+
+GRobot(isanimated::Bool=true) = GRobot(Robot(animate=isanimated))
+GRobot(sit::String, isanimated::Bool=true) = GRobot(Robot(sit, animate=isanimated))
+
+
+function HorizonSideRobots.move!(robot::GRobot, side::HorizonSide)::Bool
+    isborder(robot, side) && return false
+    move!(getbaserobot(robot), side) & return true
+end
