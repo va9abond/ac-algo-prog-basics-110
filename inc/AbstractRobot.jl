98d584cee2661f01c7ac187188907baa7a1d9c4d
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2024-12-17 19:43:41 +0300

    fix: repair putmarker!, isborder for AbstrRob

--- inc/AbstractRobot.jl
+++ inc/AbstractRobot.jl
@@ -3,8 +3,8 @@ import HorizonSideRobots as HSR
 abstract type AbstractRobot end
 
 move!(robot::AbstractRobot, side)    = HSR.move!(getbaserobot(robot), side)
-putmarker!(robot::AbstractRobot)     = HSR.putmarker!(getbaserobot(robot, side))
-isborder(robot::AbstractRobot, side) = HSR.isborder(getbaserobot(robot, side))
+putmarker!(robot::AbstractRobot)     = HSR.putmarker!(getbaserobot(robot))
+isborder(robot::AbstractRobot, side) = HSR.isborder(getbaserobot(robot), side)
 ismarker(robot::AbstractRobot)       = HSR.ismarker(getbaserobot(robot))
 temperature(robot::AbstractRobot)    = HSR.temperature(getbaserobot(robot))
 
