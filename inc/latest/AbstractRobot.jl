bc2c900bb3fd8c89dcd242a1c87b1af69257853b
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2024-12-18 21:34:19 +0300

    wip: specify side parametr type

--- inc/latest/AbstractRobot.jl
+++ inc/latest/AbstractRobot.jl
@@ -3,10 +3,10 @@ import HorizonSideRobots: move!, putmarker!, isborder, ismarker, temperature
 
 abstract type AbstractRobot end
 
-move!(robot::AbstractRobot, side)    = HSR.move!(getbaserobot(robot), side)
-putmarker!(robot::AbstractRobot)     = HSR.putmarker!(getbaserobot(robot))
-isborder(robot::AbstractRobot, side) = HSR.isborder(getbaserobot(robot), side)
-ismarker(robot::AbstractRobot)       = HSR.ismarker(getbaserobot(robot))
-temperature(robot::AbstractRobot)    = HSR.temperature(getbaserobot(robot))
+move!(robot::AbstractRobot, side::HSR.HorizonSide)     = HSR.move!(getbaserobot(robot), side)
+putmarker!(robot::AbstractRobot)                       = HSR.putmarker!(getbaserobot(robot))
+isborder(robot::AbstractRobot, side::HSR.HorizonSide)  = HSR.isborder(getbaserobot(robot), side)
+ismarker(robot::AbstractRobot)                         = HSR.ismarker(getbaserobot(robot))
+temperature(robot::AbstractRobot)                      = HSR.temperature(getbaserobot(robot))
 
 getbaserobot(robot::HSR.Robot) = robot
