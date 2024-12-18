2b9d82326200d8f0b6125838a11b93a4cdea2ef8
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2024-12-18 21:42:24 +0300

    wip: 10-mark_all_chess_n done

--- tasks/10-mark_all_chess_n.jl
+++ tasks/10-mark_all_chess_n.jl
@@ -1,32 +1,34 @@
-include("../inc/MarkCondCoordsRobot.jl")
+include("../inc/latest/GeneralRobot.jl")
+include("../inc/latest/MarkCondRobot.jl")
+include("../inc/latest/CoordsRobot.jl")
 include("../inc/roblib.jl")
 
 
-function is_in_square(N, coords::Coords{Int})::Bool
-    x = mod(coords.x, 2*N)
-    y = mod(coords.y, 2*N)
 
-    return ( (x in 0:N-1) && (y in 0:N-1) ) ||
-           ( (x in N:2*N-1) && (y in N:2*N-1) )
-end
-
-function main!(N::Int=3)
-    robot::Robot = Robot("random_pos_no_borders.sit", animate=true)
+function main!(square_size::Int = 2)
+    grobot::GRobot = GRobot("random_pos_no_borders.sit")
     corner = (Sud, West)
-    path_into_corner = move_into_corner!(robot, corner)
+    path_into_corner = move_into_corner!(grobot, corner)
 
-    # TODO how to make clousure for is_in_square
-    cond = N->is_in_square()
-    robot_main = MarkCondCoordsRobot(robot, cond)
+    rbt_coords = CoordsRobot(grobot)
+    rbt_markcond = MarkCondRobot(rbt_coords) do
+        coords = unpack(getcoords(rbt_coords))
+        n = square_size
+        x = mod(coords[1], 2*n)
+        y = mod(coords[2], 2*n)
+
+        return ( (x in 0:n-1) && (y in 0:n-1) ) ||
+               ( (x in n:2*n-1) && (y in n:2*n-1) )
+   end
 
     move_snake!(
-        robot_main;
+        rbt_markcond;
         side_move=reverse_side(corner[1]),
         side_in_row=reverse_side(corner[2])
     ) do
         false
      end
 
-    move_into_corner!(robot, corner)
-    move!(robot, reverse_path(path_into_corner))
+    move_into_corner!(grobot, corner)
+    move!(grobot, reverse_path(path_into_corner))
 end
